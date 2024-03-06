1c6ca2573e973f97058710aa3b5a116bba028276
ymktmk <73768462+ymktmk@users.noreply.github.com>
2024-03-07 01:19:58 +0900

    modify: font

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,18 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notojp = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <div className="container">{children}</div>
+      <div className={notojp.className}>{children}</div>
       <Footer />
     </>
   );
